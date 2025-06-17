28ebe4c0cab531b644a46812c48fb788711f07e9
marzia <you@example.com>
2025-06-17 16:05:32 +0200

    Implement AJAX call setup and enhance card layout with responsive design

---
+++ Javascript/main.js
@@ -0,0 +1,20 @@
+/**  Milestone 3
+Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!*/
+
+//creo una costante con l'indirizzo Api
+
+const apiPictures = "https://lanciweb.github.io/demo/api/pictures/";
+
+// recupero i nodi che mi servono per inserire il tamplate literal
+
+// devo recuperare come dati: date, title ed url.
+
+//eseguo la fetch Api
+
+// loop for per avere tutti gli oggetti e poter lavorare sul singolo
+
+// destrutturo per ottenere date, title ed url
+
+// creo markup literal in cui inserire date, title ed url.
+
+//aggiungo il markup alla DOM
